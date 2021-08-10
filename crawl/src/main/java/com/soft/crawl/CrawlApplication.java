dc6eae139284904ed7aed4358ca6570ca2667253
programuotojas <programuotojas@programuotojas.com>
2021-08-10 18:21:05 +0300

    Added swagger2

--- crawl/src/main/java/com/soft/crawl/CrawlApplication.java
+++ crawl/src/main/java/com/soft/crawl/CrawlApplication.java
@@ -3,6 +3,10 @@ package com.soft.crawl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//http://localhost:8080/swagger-ui.html
+@EnableSwagger2
 @SpringBootApplication
 public class CrawlApplication {
 
